664a018ec0d69ba16c0e9793199a3bf52c351cb4
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-10-23 15:36:55 +0900

    ルーティン設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'get_data#index'
+  resources :get_data
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
