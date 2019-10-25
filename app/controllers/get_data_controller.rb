077ed3824b44758f8329fec39307e715110899c7
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-10-25 20:49:15 +0900

    修正

--- app/controllers/get_data_controller.rb
+++ app/controllers/get_data_controller.rb
@@ -3,7 +3,10 @@ class GetDataController < ApplicationController
   end
 
   def create
-    
-
+    #binding.pry
+    agent = Mechanize.new
+    page = agent.get("https://www.amazon.co.jp/gp/product/B01HUJER0C?pf_rd_p=3d322af3-60ce-4778-b834-9b7ade73f617&pf_rd_r=101A315D3N88CRWS618Y")
+    @title = page.search('#productTitle')
+    @price = page.search('#priceblock_ourprice')
   end
 end
